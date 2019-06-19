4018797353fe9a049f5187cf819da0dfe95e51cf
Tom Scholl <Tom.scholl@Kaufland.com>
2019-06-19 16:06:24 +0200

    initial

---
+++ src/main/java/com/kaufland/demo_openshift/DemoOpenshiftApplication.java
@@ -0,0 +1,26 @@
+package com.kaufland.demo_openshift;
+
+import java.util.Arrays;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+public class DemoOpenshiftApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoOpenshiftApplication.class, args);
+    }
+
+
+
+
+
+}
+
---
+++ src/main/java/com/kaufland/demo_openshift/HelloController.java
@@ -0,0 +1,18 @@
+package com.kaufland.demo_openshift;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+            return "Greetings from Spring Boot ;) ";
+
+
+
+    }
+
+
+}
